9b84d94e58a9dfb1a5f7da43970e1e80b92a80ac
Craig <spudster14@gmail.com>
2016-11-25 12:36:02 -0500

    second class

--- Project 14/src/class1.java
+++ Project 14/src/class1.java
@@ -1,4 +1,5 @@
-
+//Craig Schein
+//Project 14
 public class class1 {
 
 	public static void main(String[] args) 
---
+++ Project 14/src/class2.java
@@ -0,0 +1,10 @@
+//Craig Schein
+//Project 14
+public class class2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("How are you today?");
+	}
+
+}
